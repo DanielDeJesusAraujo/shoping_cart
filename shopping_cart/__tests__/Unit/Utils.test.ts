6dc388e04d3200a254ab761172f271ebbce06f48
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-02-22 10:42:07 -0300

    fit: init test setup

---
+++ shopping_cart/__tests__/Unit/Utils.test.ts
@@ -0,0 +1,10 @@
+const { test, expect } = require('@jest/globals');
+const utils = require('../../src/Utils');
+
+test('should return the sum of two numbers', () => {
+  expect(true).toBe(true);
+});
+
+test('should return the difference of two numbers', () => {
+  expect().toBe();
+});
