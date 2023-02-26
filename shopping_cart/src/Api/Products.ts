58d0cbacdacf336029cf1bc824079cde2d22d068
DanielDeJesusAraujo <danielaraljo115@gmail.com>
2023-02-26 14:54:37 -0300

    fit: implements Card component and style

--- shopping_cart/src/Api/Products.ts
+++ shopping_cart/src/Api/Products.ts
@@ -3,7 +3,7 @@ import { IProduct } from '../interfaces/IProductContext';
 
 
 async function getAll(): Promise<IProduct[]> {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=roupas`;
   try {
     const response = await axios.get(url);
     return response.data.results;
--- shopping_cart/src/Components/CardProducts/Card.tsx
+++ shopping_cart/src/Components/CardProducts/Card.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import Section, { Img, Infos } from './CardStyle';
+import { Img, Infos } from './CardStyle';
 import { IProduct } from '../../interfaces/IProductContext';
+import Li from './CardStyle';
 
-function Card({product}: { product: IProduct }) {
+function Card({ product }: { product: IProduct }) {
   const { thumbnail, title, price } = product;
   return (
-    <Section>
+    <Li>
       <Img src={thumbnail} alt={title} />
       <Infos>
         <p className='tilte'>{title}</p>
-        <p className='price'>{price}</p>
+        <p className='price'>
+          {
+            price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+          }
+        </p>
       </Infos>
-    </Section>
+    </Li>
   )
 }
 
